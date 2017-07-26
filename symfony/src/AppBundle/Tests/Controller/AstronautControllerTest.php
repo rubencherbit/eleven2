d0bfd3387ff5e106e8b9b21c8816c9c568f8983a
Ruben Cherbit <rubencherbit@gmail.com>
2017-07-26 08:26:12 +0200

    unit test

---
+++ symfony/src/AppBundle/Tests/Controller/AstronautControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use GuzzleHttp\Client;
+
+class AstronautControllerTest extends WebTestCase
+{
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client = new Client([
+            'base_uri' => ''
+        ]);
+    }
+    public function testget()
+    {
+        $response = $this->client->get('/astronauts/1', [
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertArrayHasKey('id', $data);
+        $this->assertArrayHasKey('name', $data);
+        $this->assertArrayHasKey('birthdate', $data);
+        $this->assertArrayHasKey('height', $data);
+        $this->assertArrayHasKey('weight', $data);
+    }
+
+    public function testcget()
+    {
+        $response = $this->client->get('/astronauts', [
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertArrayHasKey('id', $data[0]);
+        $this->assertArrayHasKey('name', $data[0]);
+        $this->assertArrayHasKey('birthdate', $data[0]);
+        $this->assertArrayHasKey('height', $data[0]);
+        $this->assertArrayHasKey('weight', $data[0]);
+    }
+
+    // public function testpost()
+    // {
+    // }
+}
