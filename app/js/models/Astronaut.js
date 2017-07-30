dc9c21f547c87d5571861bb05770c0e226168273
Ruben C <rubencherbit@gmail.com>
2017-07-30 19:17:06 +0200

    Astronaut Model

---
+++ app/js/models/Astronaut.js
@@ -0,0 +1,75 @@
+ElevenApp.Models.Astronaut = Backbone.Model.extend({
+    url: "http://eleven2.dev/api/astronauts",
+
+    initialize: function Doc() {
+
+        this.bind("invalid", function (model, error) {
+            console.log(error);
+        });
+    },
+
+    validate: function (attributes) {
+        if (attributes.name === '') {
+            return "Name not valid";
+        }
+        if (attributes.birthdate === '') {
+            return "birthdate not valid";
+        }
+        if (attributes.height === '') {
+            return "height not valid";
+        }
+        if (attributes.weight === '') {
+            return "weight not valid";
+        }
+    },
+
+    getId: function () {
+        return this.get('id');
+    },
+    setId: function (value) {
+        this.set({
+            id: value
+        });
+    },
+
+    getName: function () {
+        return this.get('name');
+    },
+    setName: function (value) {
+        this.set({
+            name: value
+        }, {
+            validate: true
+        });
+    },
+    getBirthdate: function () {
+        return this.get('birthdate');
+    },
+    setBirthdate: function (value) {
+        this.set({
+            birthdate: value
+        }, {
+            validate: true
+        });
+    },
+    getWeight: function () {
+        return this.get('weight');
+    },
+    setWeight: function (value) {
+        this.set({
+            weight: value
+        }, {
+            validate: true
+        });
+    },
+    getHeight: function () {
+        return this.get('height');
+    },
+    setHeight: function (value) {
+        this.set({
+            height: value
+        }, {
+            validate: true
+        });
+    },
+});
